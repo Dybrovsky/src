581ec8e94296061838c6df12ac4681e88f2e96b5
Dybrovsky <sceven71@gmail.com>
2022-01-28 22:02:05 +0300

    task_5

---
+++ lesson_4/task4.js
@@ -0,0 +1,17 @@
+
+
+
+let numbers = [];
+let sumNumbers = 31;
+for (let i = 1; i <= sumNumbers; i++) {
+   numbers.push(i);
+}
+
+let days = [Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday];
+for (let i = 0; i < array.length; i++) {
+    
+    
+}
+
+
+// console.log(numbers);
